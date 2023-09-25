5770f890c3199487acbbfe3af41a720e4242f33f
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-09-25 10:52:39 -0500

    .

--- client/app/(main)/(routes)/page.tsx
+++ client/app/(main)/(routes)/page.tsx
@@ -1,7 +1,14 @@
 import { NextPage } from 'next';
+import { UserButton } from '@clerk/nextjs';
+import { ModeToggle } from '@/components/mode-toggle';
 
 const HomePage: NextPage = () => {
-  return <div>This is a protected route</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl='/' />
+      <ModeToggle />
+    </div>
+  );
 };
 
 export default HomePage;
