1e26c674c3fa137ace7c6270e952e524d3b61f81
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-09-30 13:54:54 -0500

    +

---
+++ client/app/api/servers/[serverId]/route.ts
@@ -0,0 +1,30 @@
+import { currentUser } from '@/lib/current-user';
+import { db } from '@/lib/db';
+import { NextResponse } from 'next/server';
+
+export async function PATCH(req: Request, { params }: { params: { serverId: string } }) {
+  try {
+    const user = await currentUser();
+    const { name, imageUrl } = await req.json();
+
+    if (!user) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: params.serverId,
+        userId: user.id
+      },
+      data: {
+        name,
+        imageUrl
+      }
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log('[SERVER_ID_PATCH]', error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
