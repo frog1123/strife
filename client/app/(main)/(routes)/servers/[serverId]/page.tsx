0a08d604c4181267898502bf423407423cf34f94
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-10-05 18:53:29 -0500

    Update page.tsx

--- client/app/(main)/(routes)/servers/[serverId]/page.tsx
+++ client/app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,46 @@
+import { currentUser } from '@/lib/current-user';
+import { db } from '@/lib/db';
+import { redirectToSignIn } from '@clerk/nextjs';
 import { NextPage } from 'next';
+import { redirect } from 'next/navigation';
 
-const ServerIdPage: NextPage = () => {
-  return <div>server id page</div>;
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  };
+}
+
+const ServerIdPage: NextPage<ServerIdPageProps> = async ({ params }) => {
+  const user = await currentUser();
+
+  if (!user) return redirectToSignIn();
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          userId: user.id
+        }
+      }
+    },
+    include: {
+      channels: {
+        where: {
+          name: 'general'
+        },
+        orderBy: {
+          createdAt: 'asc'
+        }
+      }
+    }
+  });
+
+  const initalChannel = server?.channels[0];
+
+  if (initalChannel?.name !== 'general') return null;
+
+  return redirect(`/servers/${params.serverId}/channels/${initalChannel.id}`);
 };
 
 export default ServerIdPage;
