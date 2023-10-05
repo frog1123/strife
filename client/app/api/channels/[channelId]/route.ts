c5bb8f8da0599ed4e56b77b733b2902b92f151ef
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-10-05 18:31:12 -0500

    +

--- client/app/api/channels/[channelId]/route.ts
+++ client/app/api/channels/[channelId]/route.ts
@@ -44,3 +44,53 @@ export async function DELETE(req: Request, { params }: { params: { channelId: st
     return new NextResponse('Internal Error', { status: 500 });
   }
 }
+
+export async function PATCH(req: Request, { params }: { params: { channelId: string } }) {
+  try {
+    const user = await currentUser();
+    const { searchParams } = new URL(req.url);
+    const { name, type } = await req.json();
+
+    const serverId = searchParams.get('serverId');
+
+    if (!user) return new NextResponse('Unauthorized', { status: 401 });
+    if (!serverId) return new NextResponse('Server ID missing', { status: 400 });
+    if (!params?.channelId) return new NextResponse('Server ID missing', { status: 400 });
+    if (name === 'general') return new NextResponse("Name cannot be 'general'", { status: 400 });
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        members: {
+          some: {
+            userId: user.id,
+            role: {
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR]
+            }
+          }
+        }
+      },
+      data: {
+        channels: {
+          update: {
+            where: {
+              id: params.channelId,
+              name: {
+                not: 'general'
+              }
+            },
+            data: {
+              name,
+              type
+            }
+          }
+        }
+      }
+    });
+
+    return NextResponse.json(server);
+  } catch (err) {
+    console.log('[CHANNEL_POST]', err);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
