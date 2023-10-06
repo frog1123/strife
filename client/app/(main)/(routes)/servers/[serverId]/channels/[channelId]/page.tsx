277f6a988e07edaea32093ccdc2cea351695d4d7
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-10-05 19:01:57 -0500

    +

--- client/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ client/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,42 @@
+import ChatHeader from '@/components/chat/chat-header';
+import { currentUser } from '@/lib/current-user';
+import { db } from '@/lib/db';
+import { redirectToSignIn } from '@clerk/nextjs';
 import { NextPage } from 'next';
+import { redirect } from 'next/navigation';
 
-const ChannelIdPage: NextPage = () => {
-  return <div>channel id page</div>;
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+const ChannelIdPage: NextPage<ChannelIdPageProps> = async ({ params }) => {
+  const user = await currentUser();
+
+  if (!user) return redirectToSignIn();
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId
+    }
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      userId: user.id
+    }
+  });
+
+  if (!channel || !member) redirect('/');
+
+  return (
+    <div className='bg-white dark:bg-[#313338] flex flex-col h-full'>
+      <ChatHeader />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
---
+++ client/components/chat/chat-header.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const ChatHeader: FC = () => {
+  return <div>chat header</div>;
+};
+
+export default ChatHeader;
